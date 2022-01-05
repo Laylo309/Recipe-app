ab88fae0a36133a8c4af6825c5df5a59460cb78f
Hamzaoutdoors <ellaouzihamza@gmail.com>
2022-01-05 21:35:49 +0100

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  root 'public_recipes#index'
+
+  root 'recipes#index'
 
   
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do

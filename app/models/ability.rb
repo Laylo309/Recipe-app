e125350bed491e7ed6844604d2c907d4608bf6c0
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2022-01-05 18:28:13 +0500

    Add ability

---
+++ app/models/ability.rb
@@ -0,0 +1,12 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, :all, public: true
+    return unless user.present?
+
+      can :manage, Food, user: user
+      can :manage, Recipe, user: user
+      can :read, :all, public: false, user: user
+    end
+end

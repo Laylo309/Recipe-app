74c571dc78b5c55852df303a19f873875f08c80c
Hamzaoutdoors <ellaouzihamza@gmail.com>
2022-01-07 19:39:50 +0100

    feat: Create elements in rails_helper file

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,5 +2,5 @@ class RecipeFood < ApplicationRecord
   belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
   belongs_to :recipe, class_name: 'Recipe', foreign_key: 'recipe_id'
 
-  validates :quantity, presence: true
+  validates :quantity, presence: true,  numericality: { greater_than: 0 }
 end
